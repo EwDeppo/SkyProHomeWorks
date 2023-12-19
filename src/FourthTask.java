b99eabbfdcb9d11b94367829c9bc614ff47be1a9
Ваня <brazik1031@icloud.com>
2023-12-19 21:27:49 +0300

    HomeWork9

---
+++ src/FourthTask.java
@@ -0,0 +1,9 @@
+public class FourthTask {
+    public static void main(String[] args){
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
+        System.out.println();
+    }
+}
