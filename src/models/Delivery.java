dec308eb4be64998413bd3e9718d1fe5406b4cf5
Ваня <brazik1031@icloud.com>
2023-12-30 15:51:17 +0300

    HomeWork11

--- src/models/Delivery.java
+++ src/models/Delivery.java
@@ -2,31 +2,13 @@ package models;
 
 public class Delivery {
     private int deliveryDistance;
-    private String deliveryMessage;
 
-    public Delivery(int deliveryDistance, String deliveryMessage) {
+    public Delivery(int deliveryDistance) {
         this.deliveryDistance = deliveryDistance;
-        this.deliveryMessage = deliveryMessage;
-    }
-
-    public String getDeliveryMessage() {
-        return deliveryMessage;
     }
 
     public int getDeliveryDistance() {
-        if (distance <= 0) {
-            System.out.println("Дистанция не может быть меньше, либо равна нулю");
-            return distance;
-        }
-        if (distance < 20) {
-            getDeliveryMessage(1);
-        } else if (distance < 60) {
-            getDeliveryMessage(2);
-        } else if (distance < 100) {
-            getDeliveryMessage(3);
-        } else {
-            System.out.println("Доставки нет");
-        }
+        return deliveryDistance;
     }
 
     public void setDeliveryDistance() {
